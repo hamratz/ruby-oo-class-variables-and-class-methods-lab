dfe85304fcafd3bd9a420e4daaa7d222bddb8c7b
Marty Warfel <marthawarfel@gmail.com>
2020-12-12 19:55:38 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,76 @@
+
+require 'pry'
+class Song
+    
+    attr_accessor :name, :artist, :genre
+    
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    
+    
+    def initialize(name, artist, genre)
+       @name = name
+       @artist = artist
+       @genre = genre
+    
+       @@count += 1
+       @@artists << artist
+       @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+            if artist_count[artist]
+                artist_count[artist] += 1
+            else
+                artist_count[artist] = 1
+            end
+        end
+        artist_count
+    end
+
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] += 1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
+    end
+
+
+
+end
+
+
+
+
+=begin
+class methods for
+.count returns the number of songs created
+.genre returns a unique array of genres of existing songs
+.artist returns a unique array of artists of existing songs
+.genre_count returns a hash {} of genres and the number of songs that have those genres
+.artist_count returns a hash {} of artists and the number of songs that have those artists
+=end
+
+
+
+
